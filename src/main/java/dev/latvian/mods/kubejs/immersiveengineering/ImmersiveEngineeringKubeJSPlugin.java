8fb0e2e54b214c87b49ead9e274c6c4b8728d19b
Max <maxh2709@gmail.com>
2022-04-11 19:45:58 +0200

    Bump project metadata, refactor recipe type IDs

--- src/main/java/dev/latvian/mods/kubejs/immersiveengineering/ImmersiveEngineeringKubeJSPlugin.java
+++ src/main/java/dev/latvian/mods/kubejs/immersiveengineering/ImmersiveEngineeringKubeJSPlugin.java
@@ -1,6 +1,19 @@
 package dev.latvian.mods.kubejs.immersiveengineering;
 
+import blusunrize.immersiveengineering.api.crafting.AlloyRecipe;
+import blusunrize.immersiveengineering.api.crafting.ArcFurnaceRecipe;
+import blusunrize.immersiveengineering.api.crafting.BlastFurnaceFuel;
+import blusunrize.immersiveengineering.api.crafting.BlastFurnaceRecipe;
+import blusunrize.immersiveengineering.api.crafting.ClocheFertilizer;
+import blusunrize.immersiveengineering.api.crafting.ClocheRecipe;
+import blusunrize.immersiveengineering.api.crafting.CokeOvenRecipe;
+import blusunrize.immersiveengineering.api.crafting.CrusherRecipe;
+import blusunrize.immersiveengineering.api.crafting.MetalPressRecipe;
+import blusunrize.immersiveengineering.api.crafting.MixerRecipe;
+import blusunrize.immersiveengineering.api.crafting.SawmillRecipe;
+import blusunrize.immersiveengineering.api.crafting.SqueezerRecipe;
 import blusunrize.immersiveengineering.api.multiblocks.MultiblockHandler;
+import blusunrize.immersiveengineering.common.util.RecipeSerializers;
 import dev.latvian.mods.kubejs.KubeJSPlugin;
 import dev.latvian.mods.kubejs.immersiveengineering.event.MultiblockFormEventJS;
 import dev.latvian.mods.kubejs.immersiveengineering.recipe.AlloyRecipeJS;
@@ -16,7 +29,6 @@ import dev.latvian.mods.kubejs.immersiveengineering.recipe.MixerRecipeJS;
 import dev.latvian.mods.kubejs.immersiveengineering.recipe.SawmillRecipeJS;
 import dev.latvian.mods.kubejs.immersiveengineering.recipe.SqueezerRecipeJS;
 import dev.latvian.mods.kubejs.recipe.RegisterRecipeHandlersEvent;
-import dev.latvian.mods.kubejs.recipe.minecraft.ShapedRecipeJS;
 import dev.latvian.mods.kubejs.script.ScriptType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -29,24 +41,24 @@ public class ImmersiveEngineeringKubeJSPlugin extends KubeJSPlugin {
 
 	@Override
 	public void addRecipes(RegisterRecipeHandlersEvent event) {
-		event.register("immersiveengineering:turn_and_copy", ShapedRecipeJS::new); // Yes this isnt the same but it should work
+		event.registerShaped(RecipeSerializers.TURN_AND_COPY_SERIALIZER.getId());
 
-		event.register("immersiveengineering:alloy", AlloyRecipeJS::new);
-		event.register("immersiveengineering:blast_furnace", BlastFurnaceRecipeJS::new);
-		event.register("immersiveengineering:blast_furnace_fuel", BlastFurnaceFuelRecipeJS::new);
-		event.register("immersiveengineering:coke_oven", CokeOvenRecipeJS::new);
-		event.register("immersiveengineering:cloche", ClocheRecipeJS::new);
-		event.register("immersiveengineering:fertilizer", ClocheFertilizerRecipeJS::new);
+		event.register(AlloyRecipe.SERIALIZER.getId(), AlloyRecipeJS::new);
+		event.register(BlastFurnaceRecipe.SERIALIZER.getId(), BlastFurnaceRecipeJS::new);
+		event.register(BlastFurnaceFuel.SERIALIZER.getId(), BlastFurnaceFuelRecipeJS::new);
+		event.register(CokeOvenRecipe.SERIALIZER.getId(), CokeOvenRecipeJS::new);
+		event.register(ClocheRecipe.SERIALIZER.getId(), ClocheRecipeJS::new);
+		event.register(ClocheFertilizer.SERIALIZER.getId(), ClocheFertilizerRecipeJS::new);
 		//event.register("immersiveengineering:blueprint", BlueprintCraftingRecipeSerializer::new);
-		event.register("immersiveengineering:metal_press", MetalPressRecipeJS::new);
-		event.register("immersiveengineering:arc_furnace", ArcFurnaceRecipeJS::new);
+		event.register(MetalPressRecipe.SERIALIZER.getId(), MetalPressRecipeJS::new);
+		event.register(ArcFurnaceRecipe.SERIALIZER.getId(), ArcFurnaceRecipeJS::new);
 		//event.register("immersiveengineering:bottling_machine", BottlingMachineRecipeSerializer::new);
-		event.register("immersiveengineering:crusher", CrusherRecipeJS::new);
-		event.register("immersiveengineering:sawmill", SawmillRecipeJS::new);
+		event.register(CrusherRecipe.SERIALIZER.getId(), CrusherRecipeJS::new);
+		event.register(SawmillRecipe.SERIALIZER.getId(), SawmillRecipeJS::new);
 		//event.register("immersiveengineering:fermenter", FermenterRecipeSerializer::new);
-		event.register("immersiveengineering:squeezer", SqueezerRecipeJS::new);
+		event.register(SqueezerRecipe.SERIALIZER.getId(), SqueezerRecipeJS::new);
 		//event.register("immersiveengineering:refinery", RefineryRecipeSerializer::new);
-		event.register("immersiveengineering:mixer", MixerRecipeJS::new);
+		event.register(MixerRecipe.SERIALIZER.getId(), MixerRecipeJS::new);
 		//event.register("immersiveengineering:mineral_mix", MineralMixSerializer::new);
 
 		//event.register("immersivepetroleum:distillation", DistillationRecipeSerializer::new);
